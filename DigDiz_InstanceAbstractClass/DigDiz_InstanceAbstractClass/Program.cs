4a055dddc5e7d80666788f14d7e9e1daacf18345
PankovAlex <pankov92@mail.ru>
2020-07-19 19:11:43 +0400

    no message

---
+++ DigDiz_InstanceAbstractClass/DigDiz_InstanceAbstractClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace DigDiz_InstanceAbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type;
+            type = typeof(MyAvatractClass);
+            ConstructorInfo Constructor = type.GetConstructor(Type.EmptyTypes);
+            object ClassObject = Constructor.Invoke(new object[] { });
+        }
+    }
+
+    public abstract class MyAvatractClass
+    {
+        public MyAvatractClass()
+        {
+
+        }
+        public static void Print()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
---
+++ DigDiz_Multithreading/DigDiz_Multithreading/DigDiz_Multithreading/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace DigDiz_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rotator rotator = new Rotator();
+            rotator.RotateAllPhoto();
+        }
+    }
+    public class Rotator
+    {
+        public void RotateAllPhoto()
+        {
+            Console.WriteLine("Enter the full path to the folder: ");
+            string FolderPath = Console.ReadLine();
+            DirectoryInfo directoryInfo = new DirectoryInfo(FolderPath);
+            if (!directoryInfo.Exists)
+            {
+                Console.WriteLine("The path to the folder is not correct");
+                return;
+            }
+            foreach (FileInfo item in directoryInfo.GetFiles())
+            {
+                Console.WriteLine("File: " + item.Name + " taken to work.");
+                Console.WriteLine(RotatePhoto(item));
+            }
+            Console.ReadKey();
+        }
+        string RotatePhoto(FileInfo File)
+        {
+            string Expansion = File.Name.Substring(File.Name.LastIndexOf('.') + 1, 3);
+            int IdThread = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            if (Expansion == "jpg" | Expansion == "png" | Expansion == "bmp")
+            {
+                try
+                {
+                    Image img = null;
+                    using (FileStream stream = new FileStream(File.FullName, FileMode.Open, FileAccess.Read))
+                    {
+                        img = Image.FromStream(stream);
+                    }
+                    if (img != null)
+                    {
+                        Stopwatch stopWatch = new Stopwatch();
+                        stopWatch.Start();
+                        img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                        img.Save(File.FullName);
+                        stopWatch.Stop();
+                        TimeSpan ts = stopWatch.Elapsed;
+                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                            ts.Hours, ts.Minutes, ts.Seconds,
+                            ts.Milliseconds / 10);
+                        return "Photo processed. Time of processing: " + elapsedTime + ". Id of the executable thread: " + IdThread;
+                    }
+                    else return "Photo not found";
+                }
+                catch (Exception)
+                {
+                    return "Mistake. Photo is not processed";
+                }
+            }
+            else return "File is not a picture";
+        }
+    }
+
+}
