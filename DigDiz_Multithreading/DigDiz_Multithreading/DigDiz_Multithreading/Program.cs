7498510b87c3cd7a37d7cab6de305967ec8e8b6d
PankovAlex <pankov92@mail.ru>
2020-07-20 20:49:28 +0400

    homework correction

--- DigDiz_Multithreading/DigDiz_Multithreading/DigDiz_Multithreading/Program.cs
+++ DigDiz_Multithreading/DigDiz_Multithreading/DigDiz_Multithreading/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Drawing;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace DigDiz_Multithreading
 {
@@ -9,13 +11,9 @@ namespace DigDiz_Multithreading
     {
         static void Main(string[] args)
         {
-            Rotator rotator = new Rotator();
-            rotator.RotateAllPhoto();
+            RotateAllPhoto();
         }
-    }
-    public class Rotator
-    {
-        public void RotateAllPhoto()
+        public static void RotateAllPhoto()
         {
             Console.WriteLine("Enter the full path to the folder: ");
             string FolderPath = Console.ReadLine();
@@ -25,48 +23,44 @@ namespace DigDiz_Multithreading
                 Console.WriteLine("The path to the folder is not correct");
                 return;
             }
-            foreach (FileInfo item in directoryInfo.GetFiles())
-            {
-                Console.WriteLine("File: " + item.Name + " taken to work.");
-                Console.WriteLine(RotatePhoto(item));
-            }
+            Parallel.ForEach(directoryInfo.GetFiles().Where(x=> 
+            x.Extension.ToUpper() == ".JPG" 
+            || x.Extension.ToUpper() == ".PNG"
+            || x.Extension.ToUpper() == ".BMP"), RotatePhoto);
             Console.ReadKey();
         }
-        string RotatePhoto(FileInfo File)
+
+        public static void RotatePhoto(FileInfo File)
         {
-            string Expansion = File.Name.Substring(File.Name.LastIndexOf('.') + 1, 3);
+            Console.WriteLine("File: " + File.Name + " taken to work.");
             int IdThread = System.Threading.Thread.CurrentThread.ManagedThreadId;
-            if (Expansion == "jpg" | Expansion == "png" | Expansion == "bmp")
+            try
             {
-                try
+                Image img = null;
+                using (FileStream stream = new FileStream(File.FullName, FileMode.Open, FileAccess.Read))
                 {
-                    Image img = null;
-                    using (FileStream stream = new FileStream(File.FullName, FileMode.Open, FileAccess.Read))
-                    {
-                        img = Image.FromStream(stream);
-                    }
-                    if (img != null)
-                    {
-                        Stopwatch stopWatch = new Stopwatch();
-                        stopWatch.Start();
-                        img.RotateFlip(RotateFlipType.Rotate90FlipX);
-                        img.Save(File.FullName);
-                        stopWatch.Stop();
-                        TimeSpan ts = stopWatch.Elapsed;
-                        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                            ts.Hours, ts.Minutes, ts.Seconds,
-                            ts.Milliseconds / 10);
-                        return "Photo processed. Time of processing: " + elapsedTime + ". Id of the executable thread: " + IdThread;
-                    }
-                    else return "Photo not found";
+                    img = Image.FromStream(stream);
                 }
-                catch (Exception)
+                if (img != null)
                 {
-                    return "Mistake. Photo is not processed";
+                    Stopwatch stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                    img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    img.Save(File.FullName);
+                    stopWatch.Stop();
+                    TimeSpan ts = stopWatch.Elapsed;
+                    string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+                    Console.WriteLine("Photo " + File.Name + " processed. Time: " + elapsedTime + ". Id thread: " + IdThread);
                 }
+                else Console.WriteLine("Photo not found");
             }
-            else return "File is not a picture";
+            catch (Exception)
+            {
+                Console.WriteLine("Mistake. Photo is not processed");
+            }
         }
     }
-
 }
+
