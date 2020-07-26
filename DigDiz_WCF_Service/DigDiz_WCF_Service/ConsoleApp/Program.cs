b0de9bf0eb92ffcfc29fa417d719bd6424b6e5dc
PankovAlex <pankov92@mail.ru>
2020-07-26 12:31:52 +0400

    Add DigDiz_WCF_Service

---
+++ DigDiz_WCF_Service/DigDiz_WCF_Service/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DigDiz_WCF_Service;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WCF_Service WCF_Service = new WCF_Service();
+            Console.WriteLine("Enter the full path to the folder: ");
+            string FolderPath = Console.ReadLine();
+            string result = WCF_Service.RotateAllPhotoInFolder(FolderPath);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DigDiz_WCF_Service/DigDiz_WCF_Service/DigDiz_WCF_Service/WCF_Service.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigDiz_WCF_Service
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "WCF_Service" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы WCF_Service.svc или WCF_Service.svc.cs в обозревателе решений и начните отладку.
+    public class WCF_Service : IWCF_Service
+    {
+        public string RotateAllPhotoInFolder(string path)
+        {
+            if (path == null) return "Missing path";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            if (!directoryInfo.Exists)
+            {
+                return "The path to the folder is not correct";
+            }
+            try
+            {
+                var files = directoryInfo.GetFiles().Where(x =>
+            x.Extension.ToUpper() == ".JPG"
+            || x.Extension.ToUpper() == ".PNG"
+            || x.Extension.ToUpper() == ".BMP");
+                Parallel.ForEach(files, RotatePhoto);
+                return "All photos are processed";
+            }
+            catch (Exception ex)
+            {
+                return "An error occurred during processing: " + ex.Message;
+            }
+
+        }
+
+        private static void RotatePhoto(FileInfo File)
+        {
+            Image img = null;
+            using (FileStream stream = new FileStream(File.FullName, FileMode.Open, FileAccess.Read))
+            {
+                img = Image.FromStream(stream);
+            }
+            if (img != null)
+            {
+                img.RotateFlip(RotateFlipType.Rotate90FlipX);
+                img.Save(File.FullName);
+            }
+        }
+    }
+}
